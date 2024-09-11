7b47b0ac7923e7c39175f17a6b9dbed5d66ef01e
AiyVan2 <ivanzky22@gmail.com>
2024-09-11 22:59:43 +0800

    Added Pause system and  visual for scenes

---
+++ Assets/Scripts/Main_Menu/Pause.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject playerControlls;
+    public GameObject pauseButton;
+    public GameObject resumeButton;
+    public GameObject mainmenuButton;
+    public GameObject quitButton;
+
+
+
+    public void showpauseButtons()
+    {
+        Time.timeScale = 0f;
+        playerControlls.SetActive(false);
+        pauseButton.SetActive(false);
+        resumeButton.SetActive(true);
+        mainmenuButton.SetActive(true);
+        quitButton.SetActive(true);
+    }
+    public void resume()
+    {
+        Time.timeScale = 1f;
+        playerControlls.gameObject.SetActive(true);
+        pauseButton.gameObject.SetActive(true);
+        resumeButton.SetActive(false);
+        mainmenuButton.SetActive(false);
+        quitButton.SetActive(false);
+    }
+    public void mainmenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
