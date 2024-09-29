aa4030fb7b77ab34531056f95438bb545b0b55cc
AiyVan2 <ivanzky22@gmail.com>
2024-09-29 18:16:25 +0800

    Playtesting

---
+++ Assets/Scripts/Transitions_Animations/Astronomy Tower/Astronomy Tower Transition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AstronomyTowerTransition : MonoBehaviour
+{
+    public Animator transitionanim;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(astronomytowertoforbiddenwingTransition());
+        }
+    }
+
+    IEnumerator astronomytowertoforbiddenwingTransition()
+    {
+        transitionanim.SetTrigger("End");
+        yield return new WaitForSeconds(transitionanim.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(10);
+    }
+}
